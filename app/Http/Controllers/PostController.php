33e14fe3f67ab8afcea025a6a36664c1c125cb51
emily <emilsrozentals@Opti-12.localdomain>
2026-02-23 11:17:17 +0200

    post can be created in db from form data

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function create(){
+        return view('posts.create');
+    }
+
+    public function store(Request $r){
+            Post::create([
+                'name' => $r->input('name'),
+                'email' => $r->input('email'),
+                'text' => $r->input('text')
+                ]);
+            return "success";
+    }
+
+    public function index(){
+        $posts = Post::all();
+
+        return view('posts.index', [
+            'posts' => $posts
+        ]);
+    }
+
+    public function show($id){
+        $post = Post::find($id);
+        return $post;
+    }
+}
---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Create new post</h1>
+
+    <form method="post" action="/posts">
+        @csrf
+        <label for="name">name:</label><br>
+        <input type="text" name="name"><br>
+
+        <label for="email">e-mail:</label><br>
+        <input type="text" name="email"><br>
+
+        <label for="text">text:</label><br>
+        <textarea name="text"></textarea><br>
+
+        <input type="submit" value="submit">
+    </form>
+</body>
+</html>
